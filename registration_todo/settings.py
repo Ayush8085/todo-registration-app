dece8be4314bf8009783b8b638a03f1d73b6b065
Ayush <ayushkumar9601@gmail.com>
2023-05-03 15:34:37 +0530

    minor changes

--- registration_todo/settings.py
+++ registration_todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_wf135@r%^*c&0_6swhs9hpyk7tyxcli+v14%@#n-p9x^#rt+2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
